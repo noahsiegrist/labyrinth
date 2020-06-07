6ff717c400754c42a91114aaac844b847934df77
Noah Siegrist <noah.siegrist@edelweiss8.ch>
2020-06-07 14:23:13 +0200

    Initial commit.

---
+++ src/components/Matrix.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="table">
+        <div class="row" v-for="(row, index) of matrix" :key="index">
+            <Tile v-for="(cell, index) of row" :key="index" v-bind:tile="cell" />
+        </div>
+    </div>
+</template>
+
+<script>
+    import Tile from './Tile'
+
+    export default {
+        name: 'Matrix',
+        components: {Tile},
+        data() {
+            return {
+                matrix:
+                    [
+                        [
+                            {
+                                right: false,
+                                bottom: false,
+                            },
+                            {
+                                right: false,
+                                bottom: false,
+                            },
+                            {
+                                right: false,
+                                bottom: false,
+                            },
+                        ],
+                        [
+                            {
+                                right: false,
+                                bottom: false,
+                            },
+                            {
+                                right: false,
+                                bottom: false,
+                            },
+                            {
+                                right: false,
+                                bottom: false,
+                            },
+                        ],
+                        [
+                            {
+                                right: false,
+                                bottom: false,
+                            },
+                            {
+                                right: false,
+                                bottom: false,
+                            },
+                            {
+                                right: false,
+                                bottom: false,
+                            },
+                        ],
+                    ],
+            };
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .table {
+        margin: auto;
+        display: table;
+        max-width: 100%;
+        width: 90vh;
+        border: 2px #c5c5c5 solid;
+
+    }
+    .row {
+        display: table-row;
+    }
+
+</style>
---
+++ src/components/Tile.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="cell" >
+        <button class="right"></button>
+        <button class="bottom"></button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Tile',
+        props: ['tile']
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    .cell {
+        position: relative;
+        display: table-cell;
+        border: 2px #c5c5c5 solid;
+    }
+
+    .cell:after {
+        content: "";
+        display: block;
+        padding-bottom: 100%;
+    }
+
+    button{
+        position: absolute;
+        opacity: 0.1;
+
+        .right {
+
+            height: 100%;
+            width: 50%;
+            right: -25%;
+            top: 0;
+        }
+        .bottom{
+            height: 50%;
+            width: 100%;
+            left: 0;
+            bottom: -25%;
+        }
+    }
+
+</style>
